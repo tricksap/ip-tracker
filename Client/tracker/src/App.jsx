f54dbdab58e5c5e0d1e818410a7403645f5c20d8
Patrick <tricksap@gmail.com>
2021-10-14 13:38:43 +0800

    first commit

---
+++ Client/tracker/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function App() {
+  return (
+    <div>
+      <div className="upper">
+        <image src=""></image>
+      </div>
+    </div>
+  );
+}
+
+export default App;
