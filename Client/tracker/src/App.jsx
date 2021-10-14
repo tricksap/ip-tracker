7e32db3bda88775ebe0be98ac879731c1726c298
Patrick <tricksap@gmail.com>
2021-10-14 14:27:38 +0800

    add map

--- Client/tracker/src/App.jsx
+++ Client/tracker/src/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import "./style.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 function App() {
   return (
     <div>
       <div className="upper">
-        <image src=""></image>
+        <image></image>
+      </div>
+      <div>
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </div>
     </div>
   );
