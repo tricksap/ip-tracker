cf37505c59b4792b515b4f6cced448fc34467180
Patrick <tricksap@gmail.com>
2021-10-16 20:28:45 +0800

    Add search bar and background images

--- Client/tracker/src/Components/Overlay.jsx
+++ Client/tracker/src/Components/Overlay.jsx
@@ -5,10 +5,23 @@ function Overlay() {
   return (
     <div className="overlay">
       <div class="flex-container">
-        <div class="flex-item-left">1</div>
-        <div class="flex-item-middle">2</div>
-        <div class="flex-item-middle">3</div>
-        <div class="flex-item-right">4</div>
+        <p class="flex-item">
+          Address <br />
+          <span>--</span>
+        </p>
+        <p class="flex-item">
+          Location <br />
+          <span>--</span>
+        </p>
+        <p class="flex-item">
+          Timezone <br />
+          <span>--</span>
+        </p>
+        <p class="flex-item">
+          ISP
+          <br />
+          <span>--</span>
+        </p>
       </div>
     </div>
   );
--- Client/tracker/src/Components/Upper.jsx
+++ Client/tracker/src/Components/Upper.jsx
@@ -4,7 +4,13 @@ import "../style.css";
 function Upper() {
   return (
     <div className="upper">
-      <h1>IP TRACKER</h1>
+      <h1>IP Address Tracker</h1>
+      <form>
+        <input type="text" />
+        <button className="button">
+          <img src={process.env.PUBLIC_URL + "icon-arrow.svg"} alt="arrow" />
+        </button>
+      </form>
     </div>
   );
 }
