0031fed8e46fb681f4d7377ac9e4fde780fc755b
Patrick <tricksap@gmail.com>
2021-11-28 17:28:52 +0800

    make it responsive

--- Client/tracker/src/Components/Leaflet.jsx
+++ Client/tracker/src/Components/Leaflet.jsx
@@ -6,7 +6,7 @@ function Leaflet(props) {
   const position = [props.Long, props.Lat];
   return (
     <div>
-      <MapContainer center={position} zoom={13} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={12} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
